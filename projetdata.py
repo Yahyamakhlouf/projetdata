08a6a45409ff22f87e79e68c8206985476f3e242
PDECM <152015957+PDECM@users.noreply.github.com>
2023-11-25 17:33:34 +0100

    Update projetdata.py

--- projetdata.py
+++ projetdata.py
@@ -1,1 +1,27 @@
+import streamlit as st
+from streamlit.logger import get_logger
 
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
