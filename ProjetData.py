ea059ecfdb39fee6fe2839e431a7813e20401d2d
PDECM <152015957+PDECM@users.noreply.github.com>
2023-12-01 16:09:06 +0100

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -3,6 +3,12 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+def from_data_file(filename):
+    url = (
+        "https://raw.githubusercontent.com/streamlit/"
+        "example-data/master/hello/v1/%s" % filename
+    )
+    return pd.read_json(url)
 
 def run():
     st.set_page_config(
@@ -21,6 +27,25 @@ def run():
     if len(str(code_postal)) != 5 and code_postal != None:
         st.warning('Veuillez entrer un code postal valide.')
 
+    st.pydeck_chart(
+            pdk.Deck(
+                map_style=None,
+                initial_view_state={
+                    "latitude": 37.76,
+                    "longitude": -122.4,
+                    "zoom": 11,
+                    "pitch": 50,
+                },
+                layers=pdk.Layer(
+            "ScatterplotLayer",
+            data=from_data_file("bart_stop_stats.json"),
+            get_position=["lon", "lat"],
+            get_color=[200, 30, 0, 160],
+            get_radius="[exits]",
+            radius_scale=0.05,
+        )
+            )
+        )
 
 if __name__ == "__main__":
     run()
