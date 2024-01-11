cb7914e074ef44f60981b40ea351b8964b001aa0
PDECM <152015957+PDECM@users.noreply.github.com>
2024-01-11 12:08:52 +0100

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -32,6 +32,10 @@ def run():
     code_postal = st.number_input('Code Postal', step=1, value=None)
 
     secteur = st.text_input('Secteur')
+
+    durée_publication = st.selectbox('Durée de publication', "Dernières 24h","Dernires 3j","Dernière semaine","Dernier mois")
+
+    télétravail = st.selectbox('Télétravail', "Télétravail possible","Téletravail partiel possible")
     
     if st.button('Valider'):
             # Vérifier si le type de contrat est vide
