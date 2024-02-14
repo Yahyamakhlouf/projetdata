86a700e139043b73264530692bbd05194cc4a921
PDECM <152015957+PDECM@users.noreply.github.com>
2024-02-14 16:36:21 +0100

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -49,7 +49,7 @@ def run():
     
     if st.button('Valider'):
             # Vérifier si le code postal est valide
-            if  code_postal == "" :
+            if not code_postal.isdigit() or len(code_postal) != 5 :
                 st.warning('Veuillez entrer un code postal valide.')
             
 
