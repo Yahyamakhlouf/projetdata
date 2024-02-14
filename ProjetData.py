caa0760576004914356abe6ed9861c5c3dcf7fbc
PDECM <152015957+PDECM@users.noreply.github.com>
2024-02-14 15:48:00 +0100

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -35,7 +35,7 @@ def run():
 
     durée_publication = st.selectbox('Durée de publication', ["Dernières 24h","Dernires 3j","Dernière semaine","Dernier mois"])
 
-    télétravail = st.selectbox('Télétravail', "Télétravail possible","Téletravail partiel possible")
+    télétravail = st.selectbox('Télétravail', ["Télétravail possible","Téletravail partiel possible"])
     
     if st.button('Valider'):
             # Vérifier si le type de contrat est vide
