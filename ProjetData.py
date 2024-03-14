7bf2eb6e2ea11cb4afe7844570258c431de95a80
PDECM <152015957+PDECM@users.noreply.github.com>
2024-03-14 21:39:24 +0100

    Update ProjetData.py

--- ProjetData.py
+++ ProjetData.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 
-#data = pd.read_json("BDD.json", lines=False)
-data2 = pd.DataFrame("table_finale_excel.xlsx")
+data = pd.read_excel("table_finale_excel.xlsx")
+data2 = pd.DataFrame(data)
 data2 = data2.rename(columns={"lieuTravail.longitude": "lon","lieuTravail.latitude": "lat"})
 data3 = data2[['lon','lat']]
 
